c9e7927ec06871c32107950f2be9e2c278bbfb34
Riley Ge <rlge@live.com>
2019-12-11 08:14:18 +0800

    Add algod client(including api and model), generated by swagger codegen.

---
+++ dotnet-algorand-sdk/Algod/Client/AlgodClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorand.Algod.Client
+{
+    /// <summary>
+    /// Rename ApiClient
+    /// </summary>
+    public class AlgodClient : ApiClient
+    {
+    }
+}
---
+++ dotnet-algorand-sdk/Algod/Client/Api/AlgodApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorand.Algod.Client.Api
+{
+    /// <summary>
+    /// Rename DefautlApi
+    /// </summary>
+    public class AlgodApi : DefaultApi
+    {
+        //public AlgodApi() : base() { }
+        //public AlgodApi(string basePath) : base(basePath) { }
+        //public AlgodApi(Configuration configuration = null) : base(configuration) { }
+        public AlgodApi(string bathPath, string apiToken)
+        {
+            Configuration config = new Configuration
+            {
+                BasePath = bathPath
+            };
+            config.ApiKey.Add(new KeyValuePair<string, string>("X-Algo-API-Token", apiToken));
+            this.Configuration = config;
+        }
+    }
+}
