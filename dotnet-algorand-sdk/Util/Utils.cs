030f56d8fc95b4bbc07679f3ff6c8eb8e8d2c669
Riley Ge <grlsr@163.com>
2019-12-19 20:45:18 +0800

    add GetRandomAssetMetaHash method. update some comments.

--- dotnet-algorand-sdk/Util/Utils.cs
+++ dotnet-algorand-sdk/Util/Utils.cs
@@ -1,8 +1,6 @@
-﻿using Algorand.Algod.Client;
-using Algorand.Algod.Client.Api;
+﻿using Algorand.Algod.Client.Api;
 using Algorand.Algod.Client.Model;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace Algorand
@@ -65,27 +63,27 @@ namespace Algorand
             return tx;
         }
         /// <summary>
-        /// 
+        /// Generate a 32 bytes string for asset metadata hash
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="name"></param>
-        /// <param name="unitName"></param>
-        /// <param name="total"></param>
-        /// <param name="trans"></param>
-        /// <param name="decimals"></param>
-        /// <param name="frozen"></param>
-        /// <param name="url"></param>
-        /// <param name="metadataHash"></param>
-        /// <param name="manager"></param>
-        /// <param name="reserve"></param>
-        /// <param name="freeze"></param>
-        /// <param name="clawback"></param>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <returns>a 32 bytes string</returns>
+        public static string GetRandomAssetMetaHash()
+        {
+            Random rd = new Random();
+            byte[] bts = new byte[24];
+            rd.NextBytes(bts);
+            var base64 = Convert.ToBase64String(bts);
+            return base64.Substring(0, 32);
+        }
+        /// <summary>
+        /// Generate a create asset transaction
+        /// </summary>
+        /// <param name="asset">The asset infomation</param>
+        /// <param name="trans">The blockchain infomation</param>
+        /// <param name="message">The message for the transaction(have no affect to the assect)</param>
+        /// <returns>transaction</returns>
         public static Transaction GetCreateAssetTransaction(AssetParams asset, TransactionParams trans, string message = "")
         {
             ValidateAsset(asset);
-
             //asset.
             //return Transaction.CreateAssetCreateTransaction(new Address(asset.Creator), trans.Fee, trans.LastRound, trans.LastRound + 1000,
             //    Encoding.UTF8.GetBytes(message), trans.GenesisID, new Digest(Convert.FromBase64String(trans.Genesishashb64)),
@@ -103,14 +101,14 @@ namespace Algorand
             return tx;
         }
         /// <summary>
-        /// 
+        /// Generate an asset config transaction
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="assetId"></param>
-        /// <param name="asset"></param>
-        /// <param name="trans"></param>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <param name="sender">The sender of the transaction, should be the manager of the asset.</param>
+        /// <param name="assetId">Asset ID</param>
+        /// <param name="asset">Asset infomation</param>
+        /// <param name="trans">The blockchain infomation</param>
+        /// <param name="message">The message for the transaction(have no affect to the assect)</param>
+        /// <returns>transaction</returns>
         public static Transaction GetConfigAssetTransaction(Address sender, ulong? assetId, AssetParams asset, TransactionParams trans, string message = "")
         {
             ValidateAsset(asset);
@@ -123,13 +121,14 @@ namespace Algorand
             return tx;
         }
         /// <summary>
-        /// 
+        /// Generate an asset activation transaction
+        /// All accounts that want recieve the new asset have to activate the asset
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="assetId"></param>
-        /// <param name="trans"></param>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <param name="sender">The account want to activate the asset</param>
+        /// <param name="assetId">Asset ID</param>
+        /// <param name="trans">The blockchain infomation</param>
+        /// <param name="message">The message for the transaction(have no affect to the assect)</param>
+        /// <returns>transaction</returns>
         public static Transaction GetActivateAssetTransaction(Address sender, ulong? assetId, TransactionParams trans, string message = "")
         {
             var tx = Transaction.CreateAssetAcceptTransaction(sender, 1, trans.LastRound,
@@ -146,6 +145,16 @@ namespace Algorand
             Account.SetFeeByFeePerByte(tx, trans.Fee);
             return tx;
         }
+        /// <summary>
+        /// Generate an asset freeze transaction
+        /// </summary>
+        /// <param name="sender">The sender of the transaction, should be the freezer of the asset.</param>
+        /// <param name="toFreeze">The account which hold the asset will be frozen.</param>
+        /// <param name="assetId">Asset ID</param>
+        /// <param name="freezeState">true to frozen the asset, false to unfrozen the asset.</param>
+        /// <param name="trans">The blockchain infomation</param>
+        /// <param name="message">The message for the transaction(have no affect to the assect)</param>
+        /// <returns>transaction</returns>
         public static Transaction GetFreezeAssetTransaction(Address sender, Address toFreeze, ulong? assetId, bool freezeState, 
             TransactionParams trans, string message = "")
         {
@@ -154,6 +163,17 @@ namespace Algorand
             Account.SetFeeByFeePerByte(tx, trans.Fee);
             return tx;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reserve"></param>
+        /// <param name="revokedFrom"></param>
+        /// <param name="receiver"></param>
+        /// <param name="assetId"></param>
+        /// <param name="amount"></param>
+        /// <param name="trans"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public static Transaction GetRevokeAssetTransaction(Address reserve, Address revokedFrom, Address receiver, ulong? assetId, 
             ulong amount, TransactionParams trans, string message = "")
         {
@@ -163,6 +183,14 @@ namespace Algorand
             Account.SetFeeByFeePerByte(tx, trans.Fee);
             return tx;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="assetId"></param>
+        /// <param name="trans"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public static Transaction GetDestroyAssetTransaction(Address manager, ulong? assetId, TransactionParams trans, string message = "")
         {
             var tx = Transaction.CreateAssetDestroyTransaction(manager, 1, trans.LastRound, trans.LastRound + 1000, 
@@ -186,7 +214,19 @@ namespace Algorand
             else if (!Address.IsValid(asset.Freezeaddr)) throw new ArgumentException("The freeze address is not valid.");
             if (asset.Clawbackaddr is null || asset.Clawbackaddr == "") asset.Clawbackaddr = asset.Managerkey;
             else if (!Address.IsValid(asset.Clawbackaddr)) throw new ArgumentException("The clawback address is not valid.");
+            if (asset.Metadatahash is null || asset.Metadatahash == "")
+                asset.Metadatahash = GetRandomAssetMetaHash();//auto generate metahash by sdk
+            else if (Encoding.UTF8.GetByteCount(asset.Metadatahash) != 32)
+                throw new ArgumentException("The metadata hash should be 32 bytes.");
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bidder"></param>
+        /// <param name="auction"></param>
+        /// <param name="signedBid"></param>
+        /// <param name="trans"></param>
+        /// <returns></returns>
         public static Transaction GetBidTransaction(Address bidder, Address auction, SignedBid signedBid, TransactionParams trans)
         {
             var tx = new Transaction(bidder, auction, 0, trans.LastRound, trans.LastRound + 1000,
@@ -197,6 +237,14 @@ namespace Algorand
             Account.SetFeeByFeePerByte(tx, trans.Fee);
             return tx;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lsig"></param>
+        /// <param name="receiver"></param>
+        /// <param name="trans"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public static Transaction GetLogicSignatureTransaction(LogicsigSignature lsig, Address receiver, TransactionParams trans, string message = "")
         {
             var tx = new Transaction(lsig.ToAddress(), receiver, 1, trans.LastRound, trans.LastRound + 1000,
