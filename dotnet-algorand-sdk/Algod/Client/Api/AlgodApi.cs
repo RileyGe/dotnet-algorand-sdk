384765430006950a14d7979a8c25ba24c27e43d2
Riley Ge <grlsr@163.com>
2019-12-21 20:18:22 +0800

    add timeout param to the api

--- dotnet-algorand-sdk/Algod/Client/Api/AlgodApi.cs
+++ dotnet-algorand-sdk/Algod/Client/Api/AlgodApi.cs
@@ -12,13 +12,17 @@ namespace Algorand.Algod.Client.Api
         //public AlgodApi() : base() { }
         //public AlgodApi(string basePath) : base(basePath) { }
         //public AlgodApi(Configuration configuration = null) : base(configuration) { }
-        public AlgodApi(string bathPath, string apiToken)
+        public AlgodApi(string bathPath, string apiToken, int timeout = -1)
         {
             Configuration config = new Configuration
             {
                 BasePath = bathPath
             };
             config.ApiKey.Add(new KeyValuePair<string, string>("X-Algo-API-Token", apiToken));
+            if(timeout > 0)
+            {
+                config.Timeout = timeout;
+            }
             this.Configuration = config;
         }
     }
