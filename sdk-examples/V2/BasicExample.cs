f15c8ac31428db506b3bde691aab16da9446a821
rileyge <grlsr@163.com>
2021-01-21 09:01:55 +0800

    update the README to add how to send transactions using API 2.0

--- sdk-examples/V2/BasicExample.cs
+++ sdk-examples/V2/BasicExample.cs
@@ -82,9 +82,7 @@ namespace sdk_examples.V2
                 // This is generally expected, but should give us an informative error message.
                 Console.WriteLine("Exception when calling algod#rawTransaction: " + e.Message);
             }
-
             Console.WriteLine("You have successefully arrived the end of this test, please press and key to exist.");
-            Console.ReadKey();
         }
     }
 }
